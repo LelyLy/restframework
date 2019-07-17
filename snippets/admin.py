78dab4674c5bf1f9ab5a0143314d7d685ba56e86
afrolova <afrolova@bekitzur.com>
2019-07-17 16:49:08 +0300

    Added Serialization

---
+++ snippets/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from snippets.models import Snippet
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date information', {'fields': ['title', 'code', 'linenos']}),
+        ('Style', {'fields': ['language', 'style']}),
+    ]
+    list_display = ('created', 'title', 'code')
+    search_fields = ['title']
+
+
+admin.site.register(Snippet, SnippetAdmin)
