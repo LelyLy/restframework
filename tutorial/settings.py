9c72c7bbf2352d417dd7d9c966de11ad1904936d
afrolova <afrolova@bekitzur.com>
2019-07-23 15:28:53 +0300

    Added ViewSets.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -29,6 +29,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tutorial.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
